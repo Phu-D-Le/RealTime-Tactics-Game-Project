a2a2f58e2af0ebd85d4277e84a384e766fc66e5e
Shibo Wang <wang20030213@gmail.com>
2024-09-14 01:07:55 -0500

    Finished menu
    except loading function

--- RT-Tactics-Game/Assets/MenuScripts/MainMenuController.cs
+++ RT-Tactics-Game/Assets/MenuScripts/MainMenuController.cs
@@ -14,10 +14,64 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private GameObject comfirmationPrompt = null;
     [SerializeField] private float defaultVolume = 1.0f;
 
+    [SerializeField] private TMP_Text mouseSenTextValue = null; 
+    [SerializeField] private Slider mouseSensSlider = null; 
+    [SerializeField] private int defaultMouseSen = 4;       
+    public int mainMouseSen = 4;
+
+    [Header("Toggle Settings")]
+    [SerializeField] private Toggle invertYToggle = null;
+
+    [Header("Graphics Settings")]
+
+    private int _qualityLevel;
+    private bool _isFullScreen;
+    private float _brightnessLevel;
+
+    [Space(10)]
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+    [SerializeField] private Toggle fullScreenToggle;
+
+
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGameDialog = null;
+
+    [Header("Resolution Dropdowns")]
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
 
     public void NewGameDialogYes()
     {
@@ -48,6 +102,49 @@ public class MainMenuController : MonoBehaviour
         volumeTextValue.text = volume.ToString("0.0");
     }
 
+    public void SetMouseSen(float sensitivity)
+    {
+        mainMouseSen = Mathf.RoundToInt(sensitivity);
+        mouseSenTextValue.text = sensitivity.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        if (invertYToggle.isOn)
+        {
+            PlayerPrefs.SetInt("masterInvertY", 1); // Invert Y
+        }
+        else
+        {
+            PlayerPrefs.SetInt("masterInvertY", 0); // Not invert
+        }
+
+        PlayerPrefs.SetFloat("masterMouseSen", mainMouseSen);
+        StartCoroutine(ConfirmationBox());
+    }
+    public void SetFullScreen(bool isFullscreen)
+    {
+        _isFullScreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        _qualityLevel = qualityIndex;
+    }
+
+    public void GraphicsApply()
+    {
+
+        PlayerPrefs.SetInt("masterQuality", _qualityLevel);
+        QualitySettings.SetQualityLevel(_qualityLevel);
+
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullScreen ? 1 : 0));
+        Screen.fullScreen = _isFullScreen;
+
+        StartCoroutine(ConfirmationBox());
+    }
+
+
     public void VolumeApply()
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
@@ -55,6 +152,24 @@ public class MainMenuController : MonoBehaviour
     }
     public void ResetButton(string MenuType)
     {
+        if (MenuType == "Graphics")
+        {
+            
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+
+            fullScreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = resolutions.Length;
+            GraphicsApply();
+
+
+        }
+
+
         if (MenuType == "Audio")
         {
             AudioListener.volume = defaultVolume;
@@ -62,6 +177,15 @@ public class MainMenuController : MonoBehaviour
             volumeTextValue.text = defaultVolume.ToString("0.0");
             VolumeApply();
         }
+        if (MenuType == "Gameplay")
+        {
+            mouseSenTextValue.text = defaultMouseSen.ToString("0");
+            mouseSensSlider.value = defaultMouseSen;
+            mainMouseSen = defaultMouseSen;
+            invertYToggle.isOn = false;
+            GameplayApply();
+        }
+
     }
 
 
