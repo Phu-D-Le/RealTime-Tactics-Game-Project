89d96310c07f444327c6d780b9160f21a17bb954
Shibo Wang <wang20030213@gmail.com>
2024-09-13 23:02:22 -0500

    sound
    sound

--- RT-Tactics-Game/Assets/MenuScripts/MainMenuController.cs
+++ RT-Tactics-Game/Assets/MenuScripts/MainMenuController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private GameObject comfirmationPrompt = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
@@ -33,4 +41,35 @@ public class MainMenuController : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+
+    public IEnumerator ConfirmationBox()
+    {
+        comfirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        comfirmationPrompt.SetActive(false);
+    }
+
 }
