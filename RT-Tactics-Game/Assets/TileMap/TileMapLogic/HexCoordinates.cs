9792e5b840e8a4f8a2908daab432952cd4e55181
ZachTheeOrr <zlo190000@utdallas.edu>
2024-10-03 19:40:50 -0500

    Movement beta
    not sigma. Need to edit button order, track valid moves, attack select, and pawn select menu

---
+++ RT-Tactics-Game/Assets/TileMap/TileMapLogic/HexCoordinates.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCoordinates : MonoBehaviour
+{
+    public static float xOffset = 1.8186f, yOffset = 4.023109f, zOffset = 1.575f;
+
+    internal Vector3Int GetHexCoords()
+        => offsetCoordinates;
+
+    [Header("Offset coordinates")]
+    [SerializeField]
+    private Vector3Int offsetCoordinates;
+
+    private void Awake()
+    {
+        offsetCoordinates = ConvertPositionOffset(transform.position);
+    }
+    private Vector3Int ConvertPositionOffset(Vector3 position)
+    {
+        int x = Mathf.CeilToInt(position.z / xOffset); // Use z-axis for x in grid
+        int y = Mathf.FloorToInt(position.y / yOffset);
+        int z = Mathf.CeilToInt(position.x / zOffset);
+
+        // Debugging output
+     Debug.Log($"World position: {position} -> Hex coordinates: ({x}, {y}, {z})");
+
+        return new Vector3Int(x, y, z);
+    }
+}
